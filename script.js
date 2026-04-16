9848b7750211b100ec7f001fd28df39d677c5120
Navpreet Kaur <navpreetchahal5504@gmail.com>
2026-04-16 11:21:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.defaultPrevented(); // stops page reload
+
+    // get user name
+    let name = document.getElementById("name").value;
+
+    // display message
+    document.getElementById("message").textContent =
+        `Thank You, ${name}! You have registered.`;
+    });
+
+
+
+    
